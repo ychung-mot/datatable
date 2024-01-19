3e48448b063d116768d73f20a1f05c666387603a
ychung-mot <young-jin.chung@dxcas.com>
2024-01-19 10:51:21 -0800

    chore

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,64 +1,21 @@
 import React from "react";
-import { useTable } from "react-table";
+import { Row, Col } from "react-bootstrap";
+import TableView from "./TableView";
 
 const DataTable = ({ columns, data }) => {
-  // Destructure table instance properties
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data });
-
   return (
-    <table {...getTableProps()} style={{ width: "100%", borderCollapse: "collapse" }}>
-      <thead style={{ borderBottom: "2px solid #ddd" }}>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  padding: "12px",
-                  textAlign: "left",
-                  fontWeight: "bold",
-                  textTransform: "capitalize",
-                  borderBottom: "2px solid #ddd",
-                }}
-              >
-                {column.render("Header")}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr
-              {...row.getRowProps()}
-              style={{
-                backgroundColor: i % 2 === 0 ? "#f9f9f9" : "white",
-              }}
-            >
-              {row.cells.map((cell) => (
-                <td
-                  {...cell.getCellProps()}
-                  style={{
-                    padding: "12px",
-                    borderBottom: "1px solid #ddd",
-                  }}
-                >
-                  {cell.render("Cell")}
-                </td>
-              ))}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <Row>
+        <Col lg={6} md={6} sm={4}>
+          <h2>Data Tables</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} style={{ zIndex: 1 }}>
+          <TableView columns={columns} data={data} />
+        </Col>
+      </Row>
+    </>
   );
 };
 
