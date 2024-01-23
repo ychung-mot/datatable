c37740e2cbe287b046f35d367633f4fd6d90de64
ychung-mot <young-jin.chung@dxcas.com>
2024-01-23 11:32:39 -0800

    SimpleBar

--- src/TableView.js
+++ src/TableView.js
@@ -1,76 +1,98 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useTable } from "react-table";
+import SimpleBar from "simplebar-react";
+import 'simplebar-react/dist/simplebar.min.css';
 
-const TableView = ({ columns, data, hiddenColumns, isFirstColSticky }) => {
+const TableView = ({
+  columns,
+  data,
+  hiddenColumns,
+  isFirstColSticky,
+  scrollHandler = () => {},
+  maxHeight = 700,
+}) => {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data, initialState: { hiddenColumns } });
+  const barRef = useRef(null);
+
+  React.useEffect(() => {
+    if (barRef.current) {
+      barRef.current
+        .getScrollElement()
+        .addEventListener("scroll", scrollHandler);
+    }
+  }, [scrollHandler]);
 
   return (
     <div>
-      <table
-        {...getTableProps()}
-        style={{ width: "100%", borderCollapse: "collapse" }}
-      >
-        <thead style={{ borderBottom: "2px solid #ddd" }}>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column, columnIndex) => (
-                <th
-                  {...column.getHeaderProps()}
-                  style={{
-                    padding: "12px",
-                    textAlign: "left",
-                    fontWeight: "bold",
-                    textTransform: "capitalize",
-                    borderBottom: "2px solid #ddd",
-                    position:
-                      isFirstColSticky && columnIndex === 0
-                        ? "sticky"
-                        : "static",
-                    left: isFirstColSticky && columnIndex === 0 ? 0 : "auto",
-                    zIndex: isFirstColSticky && columnIndex === 0 ? 1 : "auto",
-                    backgroundColor: "#fff", // Add a background color for better visibility
-                  }}
-                >
-                  {column.render("Header")}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr
-                {...row.getRowProps()}
-                style={{
-                  backgroundColor: i % 2 === 0 ? "#f9f9f9" : "white",
-                }}
-              >
-                {row.cells.map((cell, cellIndex) => (
-                  <td
-                    {...cell.getCellProps()}
+      <SimpleBar ref={barRef} autoHide={false} style={{ maxHeight }}>
+        <table
+          {...getTableProps()}
+          style={{ width: "100%", borderCollapse: "collapse" }}
+        >
+          <thead style={{ borderBottom: "2px solid #ddd" }}>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column, columnIndex) => (
+                  <th
+                    {...column.getHeaderProps()}
                     style={{
                       padding: "12px",
-                      borderBottom: "1px solid #ddd",
+                      textAlign: "left",
+                      fontWeight: "bold",
+                      textTransform: "capitalize",
+                      borderBottom: "2px solid #ddd",
                       position:
-                        isFirstColSticky && cellIndex === 0
+                        isFirstColSticky && columnIndex === 0
                           ? "sticky"
                           : "static",
-                      left: isFirstColSticky && cellIndex === 0 ? 0 : "auto",
-                      zIndex: isFirstColSticky && cellIndex === 0 ? 1 : "auto",
+                      left: isFirstColSticky && columnIndex === 0 ? 0 : "auto",
+                      zIndex:
+                        isFirstColSticky && columnIndex === 0 ? 1 : "auto",
                       backgroundColor: "#fff", // Add a background color for better visibility
                     }}
                   >
-                    {cell.render("Cell")}
-                  </td>
+                    {column.render("Header")}
+                  </th>
                 ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr
+                  {...row.getRowProps()}
+                  style={{
+                    backgroundColor: i % 2 === 0 ? "#f9f9f9" : "white",
+                  }}
+                >
+                  {row.cells.map((cell, cellIndex) => (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: "12px",
+                        borderBottom: "1px solid #ddd",
+                        position:
+                          isFirstColSticky && cellIndex === 0
+                            ? "sticky"
+                            : "static",
+                        left: isFirstColSticky && cellIndex === 0 ? 0 : "auto",
+                        zIndex:
+                          isFirstColSticky && cellIndex === 0 ? 1 : "auto",
+                        backgroundColor: "#fff", // Add a background color for better visibility
+                      }}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </SimpleBar>
     </div>
   );
 };
